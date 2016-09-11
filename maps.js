bb9b3b3c8c147ec326853040a9e1caebcbe75d0b
titoine54 <titoine54@hotmail.com>
2016-09-11 01:50:38 -0400

    setup new server.js

--- maps.js
+++ maps.js
@@ -1,7 +1,7 @@
-function myMap() {
+function theMap() {
   var mapCanvas = document.getElementById("map");
   var mapOptions = {
-    center: new google.maps.LatLng(51.5, -0.2),
+    center: new google.maps.LatLng(45.5017, -73.5673),
     zoom: 10
   }
   var map = new google.maps.Map(mapCanvas, mapOptions);
--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-var http = require('http'),
-    fs = require('fs');
+var http = require('http');
+var fs = require('fs');
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    http.createServer(function(request, response) {
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }).listen(8888);
-});
+function onRequest(request, response){
+  if (request.method == 'GET' && request.url == '/') {
+    response.writeHeader(200, {"Content-Type": "text/html"});
+    fs.createReadStream('./index.html').pipe(response);
+  } else if (request.method == 'GET' && request.url == '/maps.js') {
+      response.writeHeader(200, {"Content-Type": "text/javascript"});
+      fs.createReadStream('./maps.js').pipe(response);
+  } else if (request.method == 'GET' && request.url == 'img/fav.png') {
+      response.writeHeader(200, {"Content-Type": "image/png"});
+      fs.write('img/fav.png').pipe(response);
+  } else {
+    send404Response(response);
+  }
+}
+
+function send404Response(response){
+  response.writeHeader(404, {"Content-Type": "text/plain"});
+  response.write('Error 404 - Page not found');
+  response.end()
+}
+
+http.createServer(onRequest).listen(8888);
+console.log('Server is running ...')
